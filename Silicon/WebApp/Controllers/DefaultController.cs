31761720f907831e99e5e543a0a58a73e28f772f
Antonmarij <anton_m00@hotmail.com>
2024-03-04 18:30:14 +0100

    first commit

---
+++ Silicon/WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class DefaultController : Controller
+{
+
+    [Route("/")]
+    public IActionResult Home()
+    {
+        return View();
+    }
+
+
+}
