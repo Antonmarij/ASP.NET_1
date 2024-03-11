d659917f1c1240d551715d6f615b8d9ead69b616
Antonmarij <anton_m00@hotmail.com>
2024-03-11 21:48:52 +0100

    lektion 3

---
+++ Silicon/WebApp/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class AuthController : Controller
+{
+    #region Signup
+
+    [HttpGet]
+    [Route("/signup")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/signup")]
+    //Den här måste ha en model i sig, i detta fall SignUpViewModel
+    public IActionResult SignUp(SignUpViewModel viewModel)
+    {
+        //gör något if valid (spara i databas, redirect user)
+        if (ModelState.IsValid) 
+        {
+            
+        }
+        //annars returnera detta
+        return View(viewModel);
+    }
+
+    #endregion
+
+    #region Signin
+    [HttpGet]
+    [Route("/signin")]
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/signin")]
+    //Den här måste ha en model i sig, i detta fall SignUpViewModel
+    public IActionResult SignIn(SignInViewModel viewModel)
+    {
+        //gör något if valid (spara i databas, redirect user)
+        if (ModelState.IsValid)
+        {
+
+        }
+        //annars returnera detta
+        return View(viewModel);
+    }
+    #endregion
+}
---
+++ Silicon/WebApp/Models/SignUpViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public class SignUpViewModel
+{
+    [Display(Name = "First name", Prompt = "Enter your first name")]
+    [Required(ErrorMessage = "Your input is invalid")]
+    [MinLength(1, ErrorMessage = "Your input is invalid")]
+    [DataType(DataType.Text)]
+    public string FirstName { get; set; } = null!;
+
+
+    [Display(Name = "Last name", Prompt = "Enter your last name")]
+    [Required(ErrorMessage = "Your input is invalid")]
+    [MinLength(1, ErrorMessage = "Your input is invalid")]
+    [DataType(DataType.Text)]
+    public string LastName { get; set; } = null!;
+
+
+    [Display(Name = "Email address", Prompt = "Enter your email")]
+    [Required(ErrorMessage = "Your input is invalid")]
+    [MinLength(1, ErrorMessage = "Your input is invalid")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+
+    [Display(Name = "Password", Prompt = "Enter your password")]
+    [Required(ErrorMessage = "Your input is invalid")]
+    [MinLength(8, ErrorMessage = "Your input is invalid")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "Confirm password", Prompt = "Confirm your password")]
+    [Required(ErrorMessage = "Your input is invalid")]
+    [Compare(nameof(Password), ErrorMessage = "Password does not match")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; } = null!;
+
+    [Display(Name = "I agree to the terms & conditions")]
+    [Required(ErrorMessage = "You must accept the terms & conditions")]
+    public bool TermsAndConditions { get; set; }
+}
