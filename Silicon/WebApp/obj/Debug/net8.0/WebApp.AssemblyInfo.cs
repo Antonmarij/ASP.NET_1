bb5574e9e842d3138ba5b330a5f9fad5efc62dd9
Antonmarij <anton_m00@hotmail.com>
2024-03-13 13:34:09 +0100

    stillhome

--- Silicon/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ Silicon/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75513f3ee7001a9b33f0396bb976facf46b74a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+450d7c168f606ef05afbdb38a71e45ed31710a27")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
